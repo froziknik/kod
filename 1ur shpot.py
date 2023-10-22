def8c30bc906e8f615e80d1bc1b943200c23a0a6
student <nikitashpot43@gmail.com>
2023-10-22 10:13:56 +0300

    added small interface

--- 1ur shpot.py
+++ 1ur shpot.py
@@ -1,6 +1,20 @@
 print("Hello word")
 
-a = int(input("a:"))
-b = int(input("b:"))
+def calc() :
+    def suma() :
+        a = int(input("a:"))
+        b = int(input("b:"))
+        print(a + b)
+    def risnyca() :
+        c = int(input("a:"))
+        d = int(input("b:"))
+        print(c - d)
 
-print(a + b)
+print("Хотите выполнить отнимание?")
+otn = input("")
+if "Да" in otn :
+    risnyca()
+elif "Нет" in otn :
+    print("Отнимание не выполнено")
+else:
+    print("Неивестный символ")
